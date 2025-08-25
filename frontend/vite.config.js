d8c6c9772d29a8e0dbc562048e1fe65f91c2eca8
Helani Ambalangodage <helaniuambalangodage@gmail.com>
2025-08-25 11:22:41 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4933, //tells Vite dev server to run on port 4933 instead of default 5173
+  },
 })
